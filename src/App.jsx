9c023e75eb1db0ca06de3ef432982681cb73171a
Robbie Woolf <robbiemwoolf@gmail.com>
2023-04-26 10:38:39 -0700

    basic layout complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,55 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [aDone, setADone] = useState(false)
+  const [bDone, setBDone] = useState(false)
+  const [cDone, setCDone] = useState(false)
+  const [dDone, setDDone] = useState(false)
+  const [eDone, setEDone] = useState(false)
+  const [fDone, setFDone] = useState(false)
+
+  const [streak, setStreak] = useState(1)
+
+  const aToggle = aDone ? 'done' : null
+  const bToggle = bDone ? 'done' : null
+  const cToggle = cDone ? 'done' : null
+  const dToggle = dDone ? 'done' : null
+  const eToggle = eDone ? 'done' : null
+  const fToggle = fDone ? 'done' : null
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='container'>
+        <div className="heading">
+            <button onClick={() => setStreak((streak) => streak + 1)}><h1>{streak}/75</h1></button>
+        </div>
+        <div className="box">
+            <button className={`${aToggle}`} onClick={() => setADone(!aDone)}>water</button>
+        </div>
+        <div className="box">
+            <button className={`${bToggle}`} onClick={() => setBDone(!bDone)}>read</button>
+        </div>
+        <div className="box">
+            <button className={`${cToggle}`} onClick={() => setCDone(!cDone)}>photo</button>
+        </div>
+        <div className="box">
+            <button className={`${dToggle}`} onClick={() => setDDone(!dDone)}>outside</button>
+        </div>
+        <div className="box">
+            <button className={`${eToggle}`} onClick={() => setEDone(!eDone)}>diet</button>
+        </div>
+        <div className="box">
+            <button className={`${fToggle}`} onClick={() => setFDone(!fDone)}>exercise</button>
+        </div>
+        <div className="footer">
+            <div className="ftrbtn">
+                <button>Rules/Tips</button>
+            </div>
+            <div className="ftrbtn">
+                <button>Settings</button>
+            </div>
+        </div>
+    </div>
   )
 }
 
