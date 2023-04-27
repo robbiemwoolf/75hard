921d5b963888490e7fe4430c77564cbfe3abd764
Robbie Woolf <robbiemwoolf@gmail.com>
2023-04-27 13:26:14 -0700

    rules listed

--- src/Rules.jsx
+++ src/Rules.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 import './Home.css'
 
 function Rules() {
@@ -5,8 +6,20 @@ function Rules() {
   return (
     <div className='container'>
         <div className="heading">
-            <button><h1>75 Hard</h1></button>
+            <Link to={`/`}>
+                <button className='goHome'>
+                    <h1>75 Hard</h1>
+                    <h5>Follow these 5 simple rules every day.</h5>
+                </button>
+            </Link>            
         </div>
+        <ol className='list'>
+            <li className='rule'>Drink one gallon of water.</li>
+            <li>Read 10 pages of a nonfiction self-improvement book.</li>
+            <li>Take one progress photo.</li>
+            <li>Choose a diet. Stick to it.</li>
+            <li>Do two 45-minute workouts; one has to be outdoors.</li>
+        </ol>
     </div>
   )
 }
