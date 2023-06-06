6d84ebbc4c8cbb53b001b5f00492ce261ad2a8c4
Robbie Woolf <robbiemwoolf@gmail.com>
2023-06-06 16:15:35 -0700

    installed plugin for glsl; first shader created

---
+++ src/shaderOne.glsl
@@ -0,0 +1,35 @@
+vec3 palette( float t ) {
+        vec3 a = vec3(0.5, 0.5, 0.5);
+        vec3 b = vec3(0.5, 0.5, 0.5);
+        vec3 c = vec3(1.0, 1.0, 1.0);
+        vec3 d = vec3(0.263, 0.416, 0.557);
+        
+        return a + b*cos( 6.28318*(c*t + d) );
+}
+
+void mainImage( out vec4 fragColor, in vec2 fragCoord ) 
+{
+    // iResolution.xy is the same as creating a new vec2(iResolution.x, iResolution.y)
+    // then we multiple by 2 and subtract one to center (0,0) and keep the -1 to 1 grid
+    vec2 uv = (fragCoord * 2.0 - iResolution.xy) / iResolution.y;
+    vec2 uv0 = uv;
+    vec3 finalColor = vec3(0.0);
+    
+    for (float i = 0.0; i < 4.0; i++) {
+    
+        uv = fract(uv * 1.5) - 0.5;
+    
+        float d = length(uv) * exp(-length(uv0));
+
+        vec3 col = palette(length(uv0) + i*.4 + iTime*.4);       
+
+        d = sin(d*8. + iTime)/8.;
+        d = abs(d);
+
+        d = pow(0.01 / d, 1.2);
+
+        finalColor += col * d;
+    }  
+
+    fragColor = vec4(finalColor, 1.0);
+}
